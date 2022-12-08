94b047cd5f9ec435963e89ed89795836ffb055f2
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-12-07 20:56:47 -0500

    1.19.3

--- src/main/java/mod/azure/goldenberry/GoldenBerryMod.java
+++ src/main/java/mod/azure/goldenberry/GoldenBerryMod.java
@@ -1,20 +1,23 @@
 package mod.azure.goldenberry;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class GoldenBerryMod implements ModInitializer {
 	public static final String MODID = "goldenberry";
 
-	public static final Item GOLDENBERRY = new Item(new Item.Settings().group(ItemGroup.FOOD)
+	public static final Item GOLDENBERRY = new Item(new Item.Settings()
 			.food(new FoodComponent.Builder().alwaysEdible().saturationModifier(1.2F).hunger(6).build()));
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.ITEM, new Identifier(MODID, "golden_berries"), GOLDENBERRY);
+		Registry.register(Registries.ITEM, new Identifier(MODID, "golden_berries"), GOLDENBERRY);
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> entries.add(GOLDENBERRY));
 	}
 }
